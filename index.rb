be32080592a10452beac8a6e6d6b84cf22b2b9f3
Henrique Hefler <53583284+Kasuhira@users.noreply.github.com>
2020-01-16 16:56:17 -0300

    Fixed Linter Issues

--- index.rb
+++ index.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 def bubble_sort (array)
   size = array.size
   size.times do
-    array.each_with_index do |v,i|
-      break if i == size -1
-      array[i],array[i + 1] = array[i + 1],array[i] if v > array[i + 1]
+    array.each_with_index do |v, i|
+      break if i == size - 1
+      array[i], array[i + 1] = array[i + 1], array[i] if v > array[i + 1]
     end
   end
   array
@@ -11,13 +13,14 @@ end
 
 def bubble_sort_by(arr)
   for i in 0...arr.length
-    for j in 0...arr.length-1-i
-      if yield(arr[j],arr[j+1]).positive?
+    for j in 0...arr.length - 1 - i
+      if yield(arr[j],arr[j + 1]).positive?
         aux = arr[j]
-        arr[j] = arr[j+1]
-        arr[j+1] = aux
+        arr[j] = arr[j + 1]
+        arr[j + 1] = aux
       end
     end
   end
-  return arr
+  arr
 end
+
